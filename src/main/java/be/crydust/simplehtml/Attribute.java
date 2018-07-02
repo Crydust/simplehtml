ef47bdf29860c8a1f719e700217074f8a4fbf6f6
Kristof Neirynck <k.neirynck@gmail.com>
2018-07-02 14:06:08 +0200

    Allow namespaces in attribute names

--- src/main/java/be/crydust/simplehtml/Attribute.java
+++ src/main/java/be/crydust/simplehtml/Attribute.java
@@ -9,7 +9,7 @@ final class Attribute {
 
     // more restrictive than theoretically allowed:
     // must start with a-z and can contain dash, underscore, a-z and 0-9.
-    private static final Pattern VALID_ATTRIBUTE_NAME = Pattern.compile("(?i)[a-z][-_a-z0-9]*");
+    private static final Pattern VALID_ATTRIBUTE_NAME = Pattern.compile("(?i)(?:[a-z][-_a-z0-9]*:)?[a-z][-_a-z0-9]*");
 
     private final String name;
     private final String value;
